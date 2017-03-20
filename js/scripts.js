5741b096fb83107ad8c984caa60abc984a18f6af
philippzalewski <kontakt@centrum-ngo.pl>
2017-03-20 10:56:30 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,10 @@ var n = prompt("Podaj wysokość choinki: ");
 function rysujChoinke() {
   var star = '';
   for ( var k = 0 ; k < n; k++) {
-    for ( var i = k;  n*2-1 ; i++ ) {
+    for ( var i = k ; i <= k ; i*2-1 ) {
       star += "*";
       console.log(star);
      }
   }
 }
 rysujChoinke();
-
-
-/*
-var n = prompt("Podaj wysokość choinki: ");
-
-function rysujChoinke() {
-  var star = '';
-  for ( var k = 0 ; k < n; k++) {
-    for ( var i = k; i <= k; i++ ) {
-      star += "*";
-      console.log(star);
-     }
-  }
-}
-rysujChoinke();
-
-*/
